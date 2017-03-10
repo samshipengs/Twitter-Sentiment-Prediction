386a5efc319d348ffd0373b5c46ebfbbaccc66e0
samshipengs <samshipengs@hotmail.com>
2017-03-09 20:36:45 -0500

    stuck on writing class Data

--- temp.lua
+++
@@ -1,9 +0,0 @@
-require 'torch'
-require 'paths'
-npy4th = require 'npy4th'
-
-
--- read a .npy file into a torch tensor
-FILE_PATH = '/home/sam/Data/twitter_sentiment/'
-array = npy4th.loadnpy(FILE_PATH .. 'tweet_vecs.npy')
-data_table = npy4th.loadnpz(FILE_PATH .. 'tweet_vecs.npz')
